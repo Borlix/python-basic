b09a3b94a28f3203d05fc2dba64007897b016a83
Borlix <309441135+Borlix@users.noreply.github.com>
2026-07-29 14:21:53 +0100

    Add files via upload

---
+++ python basic part 3/string.py/indexing_and_slicing.py
@@ -0,0 +1,18 @@
+#indexing 
+str1= "Cursed with knowledge"
+print(str1[3]) 
+print(str1[6])   
+print(str1[9])
+print(str1[2])
+
+#slicing
+str = 'superBrain!'
+print(str[0:5]) 
+print(str[5:10])
+print(str[0:3])
+print(str[2:5])
+print(str[6:])
+
+#negative indexing
+str2 = 'BananaMilk'
+print(str2[-4:]) # it help us to get index of string from back side
---
+++ python basic part 3/string.py/intro_string.py
@@ -0,0 +1,18 @@
+#String = Data type which store character's
+
+str1 = "Peace!"
+str2 = 'Just keep doing it!'
+str3 = "Flower's"  #tripels dots used , so that str[6] can be writen without confuing python!
+#str3 = "'this works too! '"
+
+print(str1)
+print(str2)
+print(str3)
+
+str4 = "we are going to have a big brain!.\nwe will make cool project's \tsoon! " # \n -for next line \t -for tab
+str5 = "Its party" + " time!" #concatation - we can add the strings
+str6 = "peace"
+
+print(str4)
+print(str5)
+print(len(str6)) # prints the length of the string!
