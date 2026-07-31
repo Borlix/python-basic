6014443ecc497b43557127c8259cffbd8dd07622
Borlix <309441135+Borlix@users.noreply.github.com>
2026-07-31 02:57:58 +0100

    Add files via upload

---
+++ python basic part 3/string.py/practice.py
@@ -0,0 +1,13 @@
+# WAP to input user first name and prints its length
+
+Name = input("Enter your first name! : ")
+print(Name)
+print(len(Name))
+
+# WAP to find times of occurence of anyword in string
+
+str = " there are somthing i need to tell you !, lol i forget it "
+
+print(str.count("a"))
+print(str.count("n"))
+print(str.count("t"))
---
+++ python basic part 3/string.py/string_function.py
@@ -0,0 +1,9 @@
+#Some function's of string
+str = "i am suffering from hair fall!"
+print(str.endswith("fall!")) 
+print(str.capitalize())
+print(str.find("from"))
+print(str.replace( "from" ," with"))
+print(str.count("from"))
+
+#There are more to be learned!
